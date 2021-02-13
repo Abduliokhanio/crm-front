196a56932ee740fce5ffca10e90559aa13c34c3f
AbdulioKhanio <Akhan17@outlook.com>
2021-02-13 17:15:47 -0600

    used tables to organize leads && functionality

--- src/component/LeadsIndex.js
+++ src/component/LeadsIndex.js
@@ -1,41 +1,76 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import {getLeads} from '../actions/leadsAction'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getLeads } from "../actions/leadsAction";
+import { Table, Button } from "react-bootstrap";
 
 export class LeadsIndex extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-    constructor(){
-        super()
-        this.state = {}
-    }
+  componentDidMount() {
+    this.props.getLeads();
+  }
 
-    componentDidMount(){
-        this.props.getLeads()
+  render() {
+    let leads_arr = this.props.leads;
 
-    }
+    let pgContent = leads_arr.map((element) => {
+      return (
+        <tr>
+            <td>
+                {element.id}
+            </td>
+            <td>
+                {element.name}
+            </td>
+            <td>
+            <Button variant="info">Read</Button>
+            </td>
+            <td>
+            <Button  variant="success">Update</Button>
+            </td>
+            <td>
+            <Button variant="danger">Delete</Button>
+            </td>
+        </tr>
+      );
+    });
 
-    render() {
-
-        let leads_arr = this.props.leads
-
-        let pgContent = leads_arr.map(element => {
-            return <h1>{element.name}</h1>
-        })
-
-        return (
-            <div>
-                List of all the leads
+    let leads_table = 
+    <div>
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                <th>Lead Id</th>
+                <th>Lead Name</th>
+                <th>Read</th>
+                <th>Update</th>
+                <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
                 {pgContent}
-            </div>
-        )
-    }
+            </tbody>
+
+        </Table>
+    </div>
+
+    return (
+      <div>
+        <h1>List of all the leads</h1>
+        {leads_table}
+      </div>
+    );
+  }
 }
 
-let mapStateToProps = state => {
-    return {
-        leads: state.leadReducer.leads,
-        loading: state.leadReducer.loading
-    }
-}
+let mapStateToProps = (state) => {
+  return {
+    leads: state.leadReducer.leads,
+    loading: state.leadReducer.loading,
+  };
+};
 
-export default connect(mapStateToProps,{getLeads}) (LeadsIndex)
+export default connect(mapStateToProps, { getLeads })(LeadsIndex);
