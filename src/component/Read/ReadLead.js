3c3f116cb3cbd882771053dbb1dc49682a66eee4
AbdulioKhanio <Akhan17@outlook.com>
2021-02-22 16:19:14 -0600

    got read page up & running !

--- src/component/Read/ReadLead.js
+++ src/component/Read/ReadLead.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 
-function ReadLead() {
+function ReadLead(props) {
+    let lead_id = props.match.params.id
+
+    useEffect(() => {
+        getLead(lead_id)
+    })
+
+    let [lead, setlead] = useState([])
+
+    let getLead = (id) => {
+       fetch(`http://localhost:3001/leads/${id}`)
+        .then(resp => resp.json())
+        .then(info => {
+            setlead(info)
+        }) 
+    }
+    
+
     return (
         <div>
-            Read
+            <h1>{lead.id}</h1>
+            <h1>{lead.name}</h1>
         </div>
     )
 }
